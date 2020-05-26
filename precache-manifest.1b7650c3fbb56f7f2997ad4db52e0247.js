c2f7d13f266bb98856750ee9bba30fb3a7d2716b
Anna <bergstrom.anna@live.se>
2020-05-26 11:14:35 +0200

    Updates

---
+++ precache-manifest.1b7650c3fbb56f7f2997ad4db52e0247.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f9569a23484beac09fd7a8fb287155",
+    "url": "/reactweatherapp/index.html"
+  },
+  {
+    "revision": "2aaee48b275f92de1698",
+    "url": "/reactweatherapp/static/css/main.30dfc9fc.chunk.css"
+  },
+  {
+    "revision": "bf291d52c04006a9f39d",
+    "url": "/reactweatherapp/static/js/2.27a4ffc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactweatherapp/static/js/2.27a4ffc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaee48b275f92de1698",
+    "url": "/reactweatherapp/static/js/main.a732036c.chunk.js"
+  },
+  {
+    "revision": "562b60101dad908dd388",
+    "url": "/reactweatherapp/static/js/runtime-main.75203d17.js"
+  },
+  {
+    "revision": "a8528a868d461f09ee29a47e4c5323c8",
+    "url": "/reactweatherapp/static/media/01d.a8528a86.png"
+  },
+  {
+    "revision": "c6d1cb3616b92442a99eb3acd4063a89",
+    "url": "/reactweatherapp/static/media/02d.c6d1cb36.png"
+  },
+  {
+    "revision": "32c98f917a9796231e6f75db5c5462cb",
+    "url": "/reactweatherapp/static/media/04d.32c98f91.png"
+  },
+  {
+    "revision": "32c98f917a9796231e6f75db5c5462cb",
+    "url": "/reactweatherapp/static/media/04n.32c98f91.png"
+  },
+  {
+    "revision": "1ae9d29ba07e7ec2ba6fcb4f7f801c7d",
+    "url": "/reactweatherapp/static/media/10d.1ae9d29b.png"
+  },
+  {
+    "revision": "1ae9d29ba07e7ec2ba6fcb4f7f801c7d",
+    "url": "/reactweatherapp/static/media/10n.1ae9d29b.png"
+  },
+  {
+    "revision": "54f6821b9a9941ad54e5f3033b572df0",
+    "url": "/reactweatherapp/static/media/11d.54f6821b.png"
+  },
+  {
+    "revision": "1e64e95cf547e0c1b66673cf1b3bf676",
+    "url": "/reactweatherapp/static/media/11n.1e64e95c.png"
+  },
+  {
+    "revision": "4626d6595910a5020f7239d3ba897464",
+    "url": "/reactweatherapp/static/media/13d.4626d659.png"
+  },
+  {
+    "revision": "973f76570b0a78eef682095f47b9373f",
+    "url": "/reactweatherapp/static/media/3107.973f7657.png"
+  }
+]);
